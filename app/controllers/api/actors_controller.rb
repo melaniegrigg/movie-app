855a62de1757d60e3cd4f16c122dc9c313176f9c
melaniegrigg <melanie.grigger@gmail.com>
2020-06-30 14:53:39 -0400

    finish movie app - id method

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,6 +1,6 @@
 class Api::ActorsController < ApplicationController
   def actor_by_id
+    @actor_display_id = Actor.find_by(id: 1)
     render 'actor_info.json.jb'
-    @actor_display_id
   end
 end
